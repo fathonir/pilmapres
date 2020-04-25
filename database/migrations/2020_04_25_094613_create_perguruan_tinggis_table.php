1158a8367d216111c48f5a20b4242f1001b2ad48
indra.ramdan <indrawulida@gmail.com>
2020-04-25 17:01:55 +0700

    add model and table perguran_tinggi and program_studi

---
+++ database/migrations/2020_04_25_094613_create_perguruan_tinggis_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePerguruanTinggisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perguruan_tinggis', function (Blueprint $table) {
+            $table->char('id')->length(36);
+            $table->char('kode', 20)->default(0)->nullable();
+            $table->string('nama')->length(500)->nullable();
+            $table->string('nama_singkat')->length(200)->nullable();
+            $table->string('sk_pendirian')->length(150)->nullable();
+            $table->string('tgl_sk_pendirian')->length(15)->nullable();
+            $table->string('sk_operasional')->length(100)->nullable();
+            $table->string('tgl_sk_operasional')->length(15)->nullable();
+            $table->string('status')->length(10)->nullable();
+            $table->string('alamat_jalan')->length(250)->nullable();
+            $table->string('alamat_rt')->length(10)->nullable();
+            $table->string('alamat_rw')->length(10)->nullable();
+            $table->string('alamat_dusun')->length(250)->nullable();
+            $table->string('alamat_kelurahan')->length(250)->nullable();
+            $table->string('alamat_kode_pos')->length(10)->nullable();
+            $table->integer('kota_id')->length(10)->unsigned()->nullable();
+            $table->string('nama_kota')->length(50)->nullable();
+            $table->integer('provinsi_id')->length(10)->unsigned()->nullable();
+            $table->string('provinsi_nama')->length(50)->nullable();
+            $table->string('telepon')->length(20)->nullable();
+            $table->string('faksimile')->length(20)->nullable();
+            $table->string('website')->length(100)->nullable();
+            $table->string('email')->length(100)->nullable();
+            $table->integer('status_milik_id')->length(10)->unsigned()->nullable();
+            $table->string('status_milik_nama')->length(100)->nullable();
+            $table->char('pembina_id')->length(36)->nullable();
+            $table->string('pembina_nama')->length(50)->nullable();
+            $table->integer('bentuk_pendidikan_id')->length(10)->unsigned()->nullable();
+            $table->string('bentuk_pendidikan_nama')->length(100)->nullable();
+            $table->string('last_update')->length(20)->nullable();
+            $table->primary('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perguruan_tinggis');
+    }
+}
---
+++ database/migrations/2020_04_25_094693_create_program_studis_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProgramStudisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('program_studis', function (Blueprint $table) {
+            $table->char('id')->length(36);
+            $table->char('kode', 20)->default(0)->nullable();
+            $table->string('nama')->length(500)->nullable();
+            $table->string('status')->length(50)->nullable();
+            $table->char('id_pt')->length(36);
+            $table->char('kode _pt', 20)->default(0)->nullable();
+            $table->string('nama_pt')->length(128)->nullable();
+            $table->text('visi')->nullable();
+            $table->text('misi')->nullable();
+            $table->string('kompetensi')->length(128)->nullable();
+            $table->string('jalan')->length(128)->nullable();
+            $table->string('telepon')->length(50)->nullable();
+            $table->string('faksimile')->length(128)->nullable();
+            $table->string('website')->length(128)->nullable();
+            $table->string('email')->length(128)->nullable();
+            $table->integer('kab_kota_id')->length(15)->unsigned()->nullable();
+            $table->string('kab_kota_nama')->length(128)->nullable();
+            $table->integer('id_jenjang_didik')->length(3)->unsigned()->nullable();
+            $table->string('nama_jenjang_didik')->length(100)->nullable();
+            $table->string('tgl_berdiri')->length(20)->nullable();
+            $table->string('sk_selenggara')->length(100)->nullable();
+            $table->string('tgl_sk_selenggara')->length(20)->nullable();
+            $table->string('sks_lulus')->length(20)->nullable();
+            $table->string('kode_pos')->length(20)->nullable();
+            $table->string('last_update')->length(20)->nullable();
+            $table->primary('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('program_studis');
+    }
+}
