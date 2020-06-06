993fc361cec2e229d0e113dec28641975a46691d
Fathoni <m.fathoni@mail.com>
2020-06-06 08:34:30 +0700

    ubah enkripsi password

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use App\Group;
 use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -17,7 +18,7 @@ class UsersTableSeeder extends Seeder
         $user_admin = new User();
         $user_admin->name = 'Admin Master';
         $user_admin->email = 'admin.pilmapres@kemdikbud.go.id';
-        $user_admin->password = bcrypt('aaa123');
+        $user_admin->password = Hash::make('password');
         $user_admin->active = 1;
         $user_admin->is_user_request = 0;
         $user_admin->is_user_rejected = 0;
