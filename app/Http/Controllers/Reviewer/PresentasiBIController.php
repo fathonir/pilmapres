c42a421216f8088ead4559a4a8e8e578796911b7
Fathoni <m.fathoni@mail.com>
2020-09-10 19:12:57 +0700

    menambahkan fitur penilaian presentasi bahasa inggris

---
+++ app/Http/Controllers/Reviewer/PresentasiBIController.php
@@ -0,0 +1,236 @@
+<?php
+
+namespace App\Http\Controllers\Reviewer;
+
+use App\HasilPenilaian;
+use App\Kegiatan;
+use App\PlotReviewer;
+use App\Syarat;
+use App\Tahapan;
+use App\TahapanPeserta;
+use Auth;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use URL;
+
+class PresentasiBIController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $kegiatans = Kegiatan::with('program')->where('is_aktif', true)->get();
+        $pesertas = [];
+
+        if ($request->get('kegiatan_id')) {
+            $dosen_id = Auth::user()->dosen->id;
+            $pesertas = DB::table('plot_reviewers as pr')
+                ->join('tahapan_pesertas as tp', 'tp.id', '=', 'pr.tahapan_peserta_id')
+                ->join('pesertas as p', 'p.id', '=', 'tp.peserta_id')
+                ->join('mahasiswas as m', 'm.id', '=', 'p.mahasiswa_id')
+                ->join('program_studis as ps', 'ps.id', '=', 'm.program_studi_id')
+                ->join('perguruan_tinggis as pt', 'pt.id', '=', 'm.perguruan_tinggi_id')
+                ->where('tp.tahapan_id', Tahapan::PRESENTASI_BAHASA_INGGRIS)
+                ->where('pr.dosen_id', $dosen_id)
+                ->select('pr.id', 'm.nama', 'ps.nama_prodi', 'pt.nama_pt', 'pr.nilai_reviewer')
+                ->orderByRaw(1)
+                ->get();
+        }
+
+        return view('reviewer.presentasi-bi.index', compact('kegiatans', 'pesertas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $plot_reviewer_id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(int $plot_reviewer_id)
+    {
+        /** @var PlotReviewer $plotReviewer */
+        $plotReviewer = PlotReviewer::where('id', $plot_reviewer_id)->first();
+        /** @var TahapanPeserta $tahapanPeserta */
+        $peserta = TahapanPeserta
+            ::with([
+                'peserta',
+                'peserta.kelompokPeserta',
+                'peserta.mahasiswa',
+                'peserta.mahasiswa.programStudi',
+                'peserta.mahasiswa.perguruanTinggi'])
+            ->where('id', $plotReviewer->tahapan_peserta_id)
+            ->where('tahapan_id', Tahapan::PRESENTASI_BAHASA_INGGRIS)
+            ->first()->peserta;
+
+        // Syarat File Gagasan Kreatif
+        $syaratGK = Syarat
+            ::where(['kegiatan_id' => $peserta->kegiatan_id, 'tahapan_id' => Tahapan::BABAK_PENYISIHAN_2])
+            ->select('id')->first();
+
+        // Syarat File Poster GK
+        $syaratPoster = Syarat
+            ::where([
+                'kegiatan_id' => $peserta->kegiatan_id,
+                'tahapan_id' => Tahapan::BABAK_FINAL,
+                'nama_syarat' => 'Poster Gagasan Kreatif'])
+            ->select('id')->first();
+
+        $filePesertaPathEnv = config('app.file_peserta_path');
+        $fileGKPath = strtr($filePesertaPathEnv, [
+            '{kegiatan_id}' => $peserta->kegiatan_id,
+            '{peserta_id}' => $peserta->id,
+            '{tahapan_id}' => Tahapan::BABAK_PENYISIHAN_2,
+        ]);
+        $filePosterPath = strtr($filePesertaPathEnv, [
+            '{kegiatan_id}' => $peserta->kegiatan_id,
+            '{peserta_id}' => $peserta->id,
+            '{tahapan_id}' => Tahapan::BABAK_FINAL,
+        ]);
+
+        // File Gagasan Kreatif
+        $fileGK = $peserta->filePesertas()->where('syarat_id', $syaratGK->id)->first();
+        // File Poster GK
+        $filePoster = $peserta->filePesertas()->where('syarat_id', $syaratPoster->id)->first();
+
+        $penilaians = DB::table('plot_reviewers as pr')
+            ->select(['kp.*', 'hp.skor', 'hp.nilai'])
+            ->join('tahapan_pesertas as tp', 'tp.id', '=', 'pr.tahapan_peserta_id')
+            ->join('pesertas as p', 'p.id', '=', 'tp.peserta_id')
+            ->join('komponen_penilaians as kp', function ($join) {
+                $join->on('kp.kegiatan_id', '=', 'p.kegiatan_id');
+                $join->on('kp.tahapan_id', '=', 'tp.tahapan_id');
+                $join->on('kp.kelompok_peserta_id', '=', 'p.kelompok_peserta_id');
+            })
+            ->leftJoin('hasil_penilaians as hp', function ($join) {
+                $join->on('hp.plot_reviewer_id', '=', 'pr.id');
+                $join->on('hp.komponen_penilaian_id', '=', 'kp.id');
+            })
+            ->where('pr.id', $plot_reviewer_id)->get();
+
+        return view('reviewer.presentasi-bi.show', compact(
+            'plotReviewer', 'peserta', 'penilaians',
+            'fileGKPath', 'fileGK',
+            'filePosterPath', 'filePoster'
+        ));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $plot_reviewer_id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $plot_reviewer_id)
+    {
+        $penilaians = DB::table('plot_reviewers as pr')
+            ->select(['kp.*', 'hp.id as hp_id', 'hp.skor', 'hp.nilai'])
+            ->join('tahapan_pesertas as tp', 'tp.id', '=', 'pr.tahapan_peserta_id')
+            ->join('pesertas as p', 'p.id', '=', 'tp.peserta_id')
+            ->join('komponen_penilaians as kp', function ($join) {
+                $join->on('kp.kegiatan_id', '=', 'p.kegiatan_id');
+                $join->on('kp.tahapan_id', '=', 'tp.tahapan_id');
+                $join->on('kp.kelompok_peserta_id', '=', 'p.kelompok_peserta_id');
+            })
+            ->leftJoin('hasil_penilaians as hp', function ($join) {
+                $join->on('hp.plot_reviewer_id', '=', 'pr.id');
+                $join->on('hp.komponen_penilaian_id', '=', 'kp.id');
+            })
+            ->where('pr.id', $plot_reviewer_id)->get();
+
+        $validator = Validator::make($request->post(), [
+            'skor.*' => 'required',
+            'komentar' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect(URL::current())->withInput()->withErrors($validator);
+        }
+
+        DB::beginTransaction();
+
+        /** @var PlotReviewer $plotReviewer */
+        $plotReviewer = PlotReviewer::find($plot_reviewer_id);
+
+        $plotReviewer->nilai_reviewer = 0;
+
+        // Hasil Penilaian
+        foreach ($penilaians as $penilaian) {
+            // Jika belum ada, create
+            /** @var HasilPenilaian $hasilPenilaian */
+            if ($penilaian->hp_id == '') {
+                $hasilPenilaian = new HasilPenilaian();
+                $hasilPenilaian->plot_reviewer_id = $plot_reviewer_id;
+                $hasilPenilaian->komponen_penilaian_id = $penilaian->id;
+                $hasilPenilaian->created_at = date('Y-m-d H:i:s');
+            } else {
+                $hasilPenilaian = HasilPenilaian::find($penilaian->hp_id);
+            }
+
+            $hasilPenilaian->skor = $request->post('skor')[$penilaian->id];
+            $hasilPenilaian->nilai = $request->post('skor')[$penilaian->id] * $penilaian->bobot;
+            $hasilPenilaian->updated_at = date('Y-m-d H:i:s');
+            $hasilPenilaian->save();
+
+            $plotReviewer->nilai_reviewer += $hasilPenilaian->nilai;
+        }
+
+        // Komentar
+
+        $plotReviewer->komentar = $request->post('komentar');
+        $plotReviewer->updated_at = date('Y-m-d H:i:s');
+        $plotReviewer->save();
+
+        DB::commit();
+
+        return redirect(URL::current())->with(['alert-success' => true, 'plotReviewer' => $plotReviewer]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/reviewer/presentasi-bi/show.blade.php
@@ -0,0 +1,143 @@
+@extends('layouts.reviewer')
+@section('content')
+    <section class="content-header">
+        <h1>Presentasi Bahasa Inggris</h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-home"></i> Penilaian Final</a></li>
+            <li class="active">Presentasi Bahasa Inggris</li>
+        </ol>
+    </section>
+    <section class="content">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="box">
+                    <div class="box-body">
+
+                        @if (session('alert-success'))
+                            <div class="alert alert-success alert-dismissible">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                <h4><i class="icon fa fa-check"></i> Sukses!</h4>
+                                <p>Nilai berhasil disimpan.</p>
+                                <a href="{{ URL::to('reviewer/presentasi-bi?kegiatan_id='.$peserta->kegiatan_id) }}">Kembali ke Daftar Peserta</a>
+                            </div>
+                        @endif
+
+                        @if ($errors->count() > 0)
+                            <div class="alert alert-danger alert-dismissible">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                <h4><i class="icon fa fa-ban"></i> Gagal!</h4>
+                                Masih terdapat isian nilai yang belum lengkap. Silahkan cek isian yang berwarna merah
+                                untuk melanjutkan penilaian.
+                            </div>
+                        @endif
+
+                        <div class="form-horizontal">
+                            <div class="form-group">
+                                <label class="control-label col-md-2">Nama</label>
+                                <p class="form-control-static col-md-4">{{ $peserta->mahasiswa->nama }}</p>
+                                <label class="control-label col-md-2">Kelompok</label>
+                                <p class="form-control-static col-md-4">{{ $peserta->kelompokPeserta->kelompok_peserta }}</p>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-md-2">Prodi / PT</label>
+                                <p class="form-control-static col-md-4">
+                                    {{ $peserta->mahasiswa->programStudi->nama_prodi }} /
+                                    {{ $peserta->mahasiswa->perguruanTinggi->nama_pt }}
+                                </p>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-md-2">File Gagasan Kreatif</label>
+                                <p class="form-control-static col-md-6">
+                                    <a href="{{ URL::to($fileGKPath.'/'.$fileGK->nama_file) }}" id="fileGK"><i class="fa fa-file-pdf-o"></i> {{ $fileGK->nama_asli }}</a>
+                                    <a class="btn btn-sm btn-default" id="btnOpenFileGK"><i class="fa fa-folder-open-o"></i> Buka</a>
+                                    <a href="{{ URL::to($fileGKPath.'/'.$fileGK->nama_file) }}" class="btn btn-sm btn-default" target="_blank"><i class="fa fa-download"></i> Download</a>
+                                </p>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-md-2">File Poster</label>
+                                <p class="form-control-static col-md-6">
+                                    <a href="{{ URL::to($filePosterPath.'/'.$filePoster->nama_file) }}" id="filePoster"><i class="fa fa-file-pdf-o"></i> {{ $filePoster->nama_asli }}</a>
+                                    <a class="btn btn-sm btn-default" id="btnOpenFilePoster"><i class="fa fa-folder-open-o"></i> Buka</a>
+                                    <a href="{{ URL::to($filePosterPath.'/'.$filePoster->nama_file) }}" class="btn btn-sm btn-default" target="_blank"><i class="fa fa-download"></i> Download</a>
+                                </p>
+                            </div>
+                        </div>
+                        <a href="{{ URL::to('reviewer/presentasi-bi?kegiatan_id='.$peserta->kegiatan_id) }}"
+                           class="btn btn-default">Kembali</a>
+                    </div>
+                    <div class="box-body" id="panelPDF" style="display: none">
+                        <a class="btn btn-default btn-sm pull-right" id="btnCloseFile">Tutup</a>
+                        <iframe style="position: relative; width: 100%; height: 700px"></iframe>
+                    </div>
+                    <div class="box-body">
+                        <form method="post" action="{{ URL::current() }}">
+                            {{ method_field('PUT') }}
+                            {{ csrf_field() }}
+
+                            <table class="table table-bordered table-striped">
+                                <thead>
+                                <tr>
+                                    <th class="text-center">No</th>
+                                    <th>Dimensi Penilaian</th>
+                                    <th class="text-center" style="width: 100px">Skor</th>
+                                    <th class="text-center">Nilai</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach ($penilaians as $penilaian)
+                                    <tr>
+                                        <td class="text-center">{{ $loop->iteration }}</td>
+                                        <td>{!! $penilaian->kriteria !!}</td>
+                                        <td class="text-center">
+                                            <div class="form-group {{ $errors->first('skor.'.$penilaian->id) ? 'has-error' : '' }}" >
+                                                <input type="number" min="3" max="25" step="1" class="form-control" name="skor[{{ $penilaian->id }}]" value="{{ old('skor.'.$penilaian->id, $penilaian->skor) }}" data-id="{{ $penilaian->id }}" data-bobot="{{ $penilaian->bobot }}"/>
+                                            </div>
+                                        </td>
+                                        <td class="text-center" style="font-size: larger">
+                                            <label name="nilai[{{ $penilaian->id }}]">{{ old('skor.'.$penilaian->id) != '' ? (old('skor.'.$penilaian->id) * $penilaian->bobot) : $penilaian->nilai }}</label>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                <tr>
+                                    <td colspan="3"></td>
+                                    <td class="text-center" style="font-size: larger">
+                                        <label name="nilai_reviewer">{{ $plotReviewer->nilai_reviewer }}</label>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+
+                            <div class="form-group {{ $errors->first('komentar') ? 'has-error' : '' }}">
+                                <label class="control-label">Komentar</label>
+                                <textarea class="form-control" name="komentar" rows="4">{{ old('komentar', $plotReviewer->komentar) }}</textarea>
+                            </div>
+
+                            <div class="form-group">
+                                <a href="{{ URL::to('reviewer/presentasi-bi?kegiatan_id='.$peserta->kegiatan_id) }}" class="btn btn-default">Kembali</a>
+                                <input type="submit" class="btn btn-primary" value="Simpan" />
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+@section('js')
+    <script type="text/javascript">
+        $('#btnOpenFileGK').on('click', function() {
+            $('#panelPDF').show();
+            var filePath = $('#fileGK').attr('href');
+            $('#panelPDF').find('iframe').attr('src', filePath);
+        });
+        $('#btnOpenFilePoster').on('click', function() {
+            $('#panelPDF').show();
+            var filePath = $('#filePoster').attr('href');
+            $('#panelPDF').find('iframe').attr('src', filePath);
+        });
+        $('#btnCloseFile').on('click', function() {
+            $('#panelPDF').hide();
+        });
+    </script>
+@endsection
